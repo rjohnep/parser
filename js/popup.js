623354d8bd268de2f3cc4cc3de595054d52f49ec
John R <john@madebyewave.com>
2016-03-15 17:19:10 +0300

    item:tab in progress

--- js/popup.js
+++ js/popup.js
@@ -1,153 +1,44 @@
-function WebmailViewModel() {
+function ParserViewModel() {
     // Data
     var self = this;
     self.tabs = ['Item', 'Cart', 'Promotions'];
+    self.categories = [
+        { name: 'on Sale', value: '0'},
+        { name: 'HOT', value: '1'},
+        { name: 'Other', value: '2'},
+        { name: 'Wear', value: '3'}
+    ];
     self.chosenTabId = ko.observable();
     self.chosenTabData = ko.observable();
+    self.selectedCategory = ko.observable();
 
     // Behaviours
-    self.goToTab = function(tab) {
+    self.goToTab = function (tab) {
         self.chosenTabId(tab);
-        var mails = {
-            "mails": [{
-                "id": 44,
-                "from": "Vance \u003csigne64@wilderness.xyz\u003e",
-                "to": "steve@example.com",
-                "date": "May 4, 2011",
-                "subject": "@@ Meet new friends online @@",
-                "folder": "Spam"
-            }, {
-                "id": 45,
-                "from": "aiko.ossmith@cake-cannon.xyz",
-                "to": "steve@example.com",
-                "date": "May 16, 2011",
-                "subject": "Samuel\u0027s n0se is perfect - fix yours t0day",
-                "folder": "Spam"
-            }, {
-                "id": 46,
-                "from": "Anne \u003cbrenda.fube@space-father.xyz\u003e",
-                "to": "steve@example.com",
-                "date": "May 4, 2011",
-                "subject": "you\u0027re meds order #Y555764626",
-                "folder": "Spam"
-            }, {
-                "id": 47,
-                "from": "baker.tometo7@road-plantation.xyz",
-                "to": "steve@example.com",
-                "date": "Apr 29, 2011",
-                "subject": "Does ZZX excuse the trail?",
-                "folder": "Spam"
-            }, {
-                "id": 48,
-                "from": "brennan.linexetresson@sleet57.xyz",
-                "to": "steve@example.com",
-                "date": "May 6, 2011",
-                "subject": "FREE iPAD 2 GUARANTEED",
-                "folder": "Spam"
-            }, {
-                "id": 49,
-                "from": "Charlotte \u003ckasimir@wrenchmarble4.xyz\u003e",
-                "to": "steve@example.com",
-                "date": "May 7, 2011",
-                "subject": "no other product can help",
-                "folder": "Spam"
-            }, {
-                "id": 50,
-                "from": "Deanna Wridaypa \u003cluke.pytocooson@mitten.xyz\u003e",
-                "to": "steve@example.com",
-                "date": "May 1, 2011",
-                "subject": "!!Winner!! Place first 1000USD bet free",
-                "folder": "Spam"
-            }, {
-                "id": 51,
-                "from": "Hamilton Yueehi \u003cjameson.yuhapa@income-family.xyz\u003e",
-                "to": "steve@example.com",
-                "date": "May 10, 2011",
-                "subject": "2 unread message(s): Hello Taylor",
-                "folder": "Spam"
-            }, {
-                "id": 52,
-                "from": "hasad.lineeesson1@camp-stew.xyz",
-                "to": "steve@example.com",
-                "date": "May 20, 2011",
-                "subject": "March United Bank Inc",
-                "folder": "Spam"
-            }, {
-                "id": 53,
-                "from": "HGLF \u003canastasia7@tree-hook.xyz\u003e",
-                "to": "steve@example.com",
-                "date": "May 4, 2011",
-                "subject": "YouPharmacy5 special offer: 80% off this week only",
-                "folder": "Spam"
-            }, {
-                "id": 54,
-                "from": "Isabella Hasmith \u003clinda.tocoovaford@bedroomcactus6.xyz\u003e",
-                "to": "steve@example.com",
-                "date": "Apr 27, 2011",
-                "subject": "Ticket confirmation #484993598",
-                "folder": "Spam"
-            }, {
-                "id": 55,
-                "from": "jackson.passon@frogbreakfast.xyz",
-                "to": "steve@example.com",
-                "date": "May 7, 2011",
-                "subject": "УДВОЕНИЕ СКОРОСТИ ЧТЕНИЯ ЗА 3 ЧАСА",
-                "folder": "Spam"
-            }, {
-                "id": 56,
-                "from": "leandra@advice.xyz",
-                "to": "steve@example.com",
-                "date": "May 7, 2011",
-                "subject": "Stunning replica watches",
-                "folder": "Spam"
-            }, {
-                "id": 57,
-                "from": "russell.drison@cannoncoast1.xyz",
-                "to": "steve@example.com",
-                "date": "May 26, 2011",
-                "subject": "RE: Hi Murphy...",
-                "folder": "Spam"
-            }, {
-                "id": 58,
-                "from": "william.boam@throat-horn.xyz",
-                "to": "steve@example.com",
-                "date": "May 16, 2011",
-                "subject": "An exclusive offer from your bank",
-                "folder": "Spam"
-            }, {
-                "id": 59,
-                "from": "zephania.va@crookvolcano55.xyz",
-                "to": "steve@example.com",
-                "date": "May 5, 2011",
-                "subject": "Днепропетровск",
-                "folder": "Spam"
-            }, {
-                "id": 60,
-                "from": "ZK \u003cjoshua@sun-money.xyz\u003e",
-                "to": "steve@example.com",
-                "date": "May 12, 2011",
-                "subject": "Somebody invited you to join eReaders Community",
-                "folder": "Spam"
-            }]
-        };
-        //self.mails = mails;
-        self.chosenTabData(mails);
-        //$.ajax({
-        //        method: "POST",
-        //        url: "mail.php",
-        //        data: {name: "John", location: "Boston"},
-        //        dataType: 'json'
-        //    })
-        //    .done(function (msg) {
-        //        debugger;
-        //    })
-        //    .always(function (msg) {
-        //        debugger;
-        //    });
-        //$.get('/mail.php', { tab: tab }, self.chosenTabData);
+
+        $.ajax({
+                method: "POST",
+                url: "http://dev3.madebyewave.com/mail.php",
+                data: {tab: tab},
+                dataType: 'json'
+            })
+            .done(function (msg) {
+                //debugger;
+                self.chosenTabData({
+                    item: {
+                        title: 'AGENT BLACK GLOVE',
+                        price: '199',
+                        size: '37',
+                        qty: '2',
+                        src: 'http://www.sandler.com.au/media/catalog/product/cache/1/image/1800x/966f5e480dc04e44279dd569854e5263/s/a/sandler_agent_blackglove_1.jpg'
+                    }
+                });
+                //self.chosenTabData(msg);
+            });
     };
 
+    //open default tab
     self.goToTab('Item');
 }
 
-ko.applyBindings(new WebmailViewModel());
+ko.applyBindings(new ParserViewModel());
--- mail.php
+++ mail.php
@@ -1,10 +1,14 @@
 <?php
 header('Content-Type: application/json');
+$key = strtolower(htmlspecialchars($_POST['tab']));
+$result = [
+    'item' => ['title' => 'Product title', 'price' => '199'],
+    'cart' => [
+        [
+            'id' => "10",
+            'title' =>  "Product title"
+        ]
+    ]
+];
 
-
-$mails = json_encode(array(
-    'from' => '1',
-    'to' => '2'
-));
-
-echo $mails;
+echo json_encode([$key => $result[$key]]);
